77d947a7cd55c103d4346fb059cfb93f20991844
Owen Mooney <owen.m@bccriskadvisory.com>
2015-11-30 12:15:41 +0000

    forced run script to clean up existing bootstrap script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cd /usr/local/bin
+rm chef-bootstrap
 wget 'https://raw.githubusercontent.com/BCCRiskAdvisory/chef-bootstrap/master/chef-bootstrap'
 chmod a+x /usr/local/bin/chef-bootstrap
 chef-bootstrap
