17f0b09764338a4087ff6fae10c9152ae0c61c95
Owen Mooney <owen.m@bccriskadvisory.com>
2015-11-30 12:28:37 +0000

    changed parameter order

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd /usr/local/bin
 rm chef-bootstrap
-wget "https://raw.githubusercontent.com/BCCRiskAdvisory/chef-bootstrap/master/chef-bootstrap?t=`date +'%s'`" -O chef-bootstrap
+wget -O chef-bootstrap "https://raw.githubusercontent.com/BCCRiskAdvisory/chef-bootstrap/master/chef-bootstrap?t=`date +'%s'`" 
 chmod a+x /usr/local/bin/chef-bootstrap
 chef-bootstrap
