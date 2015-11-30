83e3114536227eadd5ccebd8e0edd4947d3b1ef4
Owen Mooney <owen.m@bccriskadvisory.com>
2015-11-30 12:27:34 +0000

    changed wget command

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd /usr/local/bin
 rm chef-bootstrap
-wget "https://raw.githubusercontent.com/BCCRiskAdvisory/chef-bootstrap/master/chef-bootstrap?t=`date +'%s'`" -o chef-bootstrap
+wget "https://raw.githubusercontent.com/BCCRiskAdvisory/chef-bootstrap/master/chef-bootstrap?t=`date +'%s'`" -O chef-bootstrap
 chmod a+x /usr/local/bin/chef-bootstrap
 chef-bootstrap
