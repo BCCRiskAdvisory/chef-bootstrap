d993be7ff882d013045f5a28f0e6ed396320dbc8
Owen Mooney <owen.m@bccriskadvisory.com>
2015-11-30 12:23:56 +0000

    added cache buster to bootstrap url

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd /usr/local/bin
 rm chef-bootstrap
-wget 'https://raw.githubusercontent.com/BCCRiskAdvisory/chef-bootstrap/master/chef-bootstrap'
+wget "https://raw.githubusercontent.com/BCCRiskAdvisory/chef-bootstrap/master/chef-bootstrap?t=`date +'%s'`"
 chmod a+x /usr/local/bin/chef-bootstrap
 chef-bootstrap
